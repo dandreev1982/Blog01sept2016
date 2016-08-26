d1664d6ad72bd7ba48179711c2f96e952a9f15fc
dandreev1982 <dandreev@mail.bg>
2016-08-26 12:24:47 +0300

    Some minor changes
    Hide/Show buttons(Редакция, детайли,)

--- MyBlog1sept2016/MyBlog1sept2016/Controllers/HomeController.cs
+++ MyBlog1sept2016/MyBlog1sept2016/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace MyBlog1sept2016.Controllers
         public ActionResult Index()
         {
             var db = new ApplicationDbContext();
-            var post = db.Posts.OrderByDescending(p => p.Date).Take(5);
+            var post = db.Posts.OrderByDescending(p => p.Date).Take(3);
             return View(post.ToList());
         }
 
